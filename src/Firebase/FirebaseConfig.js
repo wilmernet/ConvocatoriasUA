ec2fbcc58bb303132321d4fb774a9739b368aedb
wilmernet <wilmer@uniamazonia.edu.co>
2025-06-03 13:18:19 -0500

    environment variables

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+//import { VITE_FIREBASE__API_KEY, VITE_FIREBASE__AUTH_DOMAIN, VITE_FIREBASE__DATABASE_URL, VITE_FIREBASE__PROJECT_ID, VITE_FIREBASE__STORAGE_BUCKET, VITE_FIREBASE__MESSAGING_SENDER_ID, VITE_FIREBASE__APP_ID } from "@env/vite.env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQfAvCSDjVpV9c01cCgSxVRQj4GfdNb-4",
-  authDomain: "modulosudla-75270.firebaseapp.com",
-  projectId: "modulosudla-75270",
-  storageBucket: "modulosudla-75270.firebasestorage.app",
-  messagingSenderId: "497075319825",
-  appId: "1:497075319825:web:325966dcc2b4b357e7adb0"
+  apiKey: import.meta.env.VITE_FIREBASE__API_KEY, // Your web app's Firebase configuration
+  authDomain: import.meta.env.VITE_FIREBASE__AUTH_DOMAIN, // Your web app's Firebase configuration
+  databaseURL: import.meta.env.VITE_FIREBASE__DATABASE_URL, // Your web app's Firebase configuration
+  projectId: import.meta.env.VITE_FIREBASE__PROJECT_ID, // Your web app's Firebase configuration
+  storageBucket: import.meta.env.VITE_FIREBASE__STORAGE_BUCKET, // Your web app's Firebase configuration
+  messagingSenderId: import.meta.env.VITE_FIREBASE__MESSAGING_SENDER_ID, // Your web app's Firebase configuration
+  appId: import.meta.env.VITE_FIREBASE__APP_ID, // Your web app's Firebase configuration
 };
 
 // Initialize Firebase
